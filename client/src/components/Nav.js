788aa870d380d32a3caf573a2cecc7a1f05b0de0
Brandon Turner <brandon88@gmail.com>
2018-10-16 20:32:54 -0700

    added nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -25,7 +25,7 @@ export default class Nav extends React.Component {
     </div>
   </li>
 </ul>
-<div class="tab-content" id="myTabContent">
+{/* <div class="tab-content" id="myTabContent">
   <div class="tab-pane fade show active" id="home">
     <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
   </div>
@@ -37,8 +37,7 @@ export default class Nav extends React.Component {
   </div>
   <div class="tab-pane fade" id="dropdown2">
     <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater.</p>
-  </div>
-</div>
+  </div> */}
 </div>
     );
   }
