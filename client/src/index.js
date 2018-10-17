d162e884ab37974834efc100d5d9234835d4f440
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-10-16 20:38:13 -0700

    this commit includes files for auth

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
