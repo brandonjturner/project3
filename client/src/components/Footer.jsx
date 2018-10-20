7a2debf5ff5bb568b780c393eb815d9cff081285
Brandon Turner <brandon88@gmail.com>
2018-10-20 11:20:20 -0700

    nav/footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {    
+    return (
+
+<footer>
+
+    <div class="footer-copyright text-center py-3">
+    © 2018 Copyright
+    </div>
+
+</footer>
+        
+    );
+  }
+}
