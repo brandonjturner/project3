0ebf682cdf79a1e36a1a235df406640b63407e7c
mcarreon <carryon2095@gmail.com>
2018-11-07 03:36:49 -0800

    took build out of gitignore

---
+++ client/build/precache-manifest.b3023b08b55194c8f19bc5d2de78bf41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e64f51c02268ba72b6af0f6f5a7f22c2",
+    "url": "/static/media/marlo.e64f51c0.jpg"
+  },
+  {
+    "revision": "ba1b058d6f1765a70e0cd935f351ad41",
+    "url": "/static/media/cyrus.ba1b058d.jpg"
+  },
+  {
+    "revision": "67907428f4caa5929a494e4c7e2b8fb0",
+    "url": "/static/media/bruce.67907428.jpg"
+  },
+  {
+    "revision": "73a8843d84650b3ff6eab170beaa767f",
+    "url": "/static/media/brandon.73a8843d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e44a6c97085234f311da",
+    "url": "/static/js/main.e44a6c97.chunk.js"
+  },
+  {
+    "revision": "29705f83bb6882f284e5",
+    "url": "/static/js/1.29705f83.chunk.js"
+  },
+  {
+    "revision": "e44a6c97085234f311da",
+    "url": "/static/css/main.14a0befa.chunk.css"
+  },
+  {
+    "revision": "666b5e0bd18e832a60c2bebb970da8b2",
+    "url": "/index.html"
+  }
+];
