8feff092fe1420430160f8c8ed51ee27918964cb
mcarreon <carryon2095@gmail.com>
2018-11-07 03:44:59 -0800

    changed production env to /build

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(passport.session()) // will call the deserializeUser
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, '../build/static')))
+	app.use('/static', express.static(path.join(__dirname, '../build')))
 	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname, '../build/'))
 	})
