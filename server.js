ac31068eed7f52d0d23d5f24935080757786db5b
mcarreon <carryon2095@gmail.com>
2018-11-07 22:12:08 -0800

    working on heroku

--- server.js
+++ server.js
@@ -70,9 +70,9 @@ app.use( (req, res, next) => {
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, 'client/build/static')))
+	app.use(express.static(path.join(__dirname, "client", "build")))
 	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../build/static/index.html'))
+		res.sendFile(path.join(__dirname, 'static/index.html'))
 	})
 }
 
