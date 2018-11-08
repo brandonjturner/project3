d6546298f5430c9a6184ad2ff9f9a22c11b84d35
mcarreon <carryon2095@gmail.com>
2018-11-07 22:48:05 -0800

    server

--- server.js
+++ server.js
@@ -71,6 +71,10 @@ if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
 	app.use(express.static(path.join(__dirname, "client", "build")))
+	app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname, 'static/index.html'))
+	})
+
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'static/index.html'))
 	})
