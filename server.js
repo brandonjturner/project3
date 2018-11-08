46dd2e42e71aa527e4d6597957591bb7046648f5
mcarreon <carryon2095@gmail.com>
2018-11-07 22:56:33 -0800

    reverting master once again

--- server.js
+++ server.js
@@ -70,11 +70,8 @@ app.use( (req, res, next) => {
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, "client", "build")))
+	app.use(express.static(path.join(__dirname, "client", "build")))
 	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, 'static/index.html'))
-	})
-	app.get('/auth', (req, res) => {
 		res.sendFile(path.join(__dirname, 'static/index.html'))
 	})
 }
