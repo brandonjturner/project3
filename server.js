0730275442bdcd195667594851c7b64eaaf41268
mcarreon <carryon2095@gmail.com>
2018-11-07 22:43:17 -0800

    working on heroku

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
 	app.use(express.static(path.join(__dirname, "client", "build")))
-	app.get('/', (req, res) => {
+	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'static/index.html'))
 	})
 }
