0b977c22f3c754b9fb789a82263939bea40628d0
mcarreon <carryon2095@gmail.com>
2018-11-07 22:50:03 -0800

    server

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ if (process.env.NODE_ENV === 'production') {
 		res.sendFile(path.join(__dirname, 'static/index.html'))
 	})
 
-	app.get('*', (req, res) => {
+	app.get('/*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'static/index.html'))
 	})
 }
